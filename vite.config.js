be3f701b780fedb5978b805a26399632e5072e3a
Mohameed-sayed <mohammeedelsayeed08@gmail.com>
2024-11-04 18:56:52 +0200

    E-commerce

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Root-E-commerce/', // Add this line to specify the base path
+  
   plugins: [react()],
 })
